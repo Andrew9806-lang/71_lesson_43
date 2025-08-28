c325d00ac2fbd93db828f9f66af4fc147181b574
Andrew9806-lang <kornilovaamina20@gmail.com>
2025-08-28 21:58:34 +0300

    add new class - test2

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("eto pervaya versiya");
+        System.out.println("Obnovleniya klassa Test1");
     }
 }
